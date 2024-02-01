cdd9111cd3f18c97126d247395dd18ae31912ff8
Pedro <128763848+pedroBSil@users.noreply.github.com>
2024-02-01 16:51:20 -0300

    Add files via upload

---
+++ JPCAR/JPCAR/Estacionamento.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JPCAR
+{
+    internal class Estacionamento
+    {
+        private decimal precoInicial;
+        private decimal precoHora;
+        private List<string> veiculos;
+
+        public Estacionamento(decimal precoInicial, decimal precoHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+            this.veiculos = new List<string>();
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veiculo:");
+            string placa = Console.ReadLine().ToUpper();
+
+            if (veiculos.Contains(placa))
+            {
+                Console.WriteLine("Já existe um veiculo com essa placa.");
+            }
+            else
+            {
+                veiculos.Add(placa);
+                Console.WriteLine("Veiculo cadastrado com sucesso.");
+            }
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veiculo:");
+            string placa = Console.ReadLine().ToUpper();
+
+            if (veiculos.Contains(placa))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo ficou no pátio:");
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal precoTotal = precoInicial + precoHora * horas;
+                veiculos.Remove(placa);
+                Console.WriteLine($"O veículo de placa: {placa} foi removido e deve pagar {precoTotal}.");
+            }
+            else
+            {
+                Console.WriteLine("O veiculo não foi encontrado no pátio.");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veiculos estacionados são:");
+                foreach (var v in veiculos)
+                {
+                    Console.WriteLine(v);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos no pátio.");
+            }
+        }
+    }
+}
---
+++ JPCAR/JPCAR/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace JPCAR
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal precoInicial = 0;
+            decimal precoHora = 0;
+
+            Console.WriteLine("Seja bem-vindo ao Estacionamento do João");
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("Digite o preço Inicial");
+            precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Agora digite o preço por hora");
+            precoHora = Convert.ToDecimal(Console.ReadLine());
+
+            Estacionamento es = new Estacionamento(precoInicial, precoHora);
+
+            bool menu = true;
+
+            while (menu)
+            {
+                Console.Clear();
+                Console.WriteLine("Escolha uma opção");
+                Console.WriteLine("1 - Adicionar Veículo");
+                Console.WriteLine("2 - Remover Veículo");
+                Console.WriteLine("3 - Listar Veículos");
+                Console.WriteLine("4 - Sair");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        es.AdicionarVeiculo();
+                        break;
+
+                    case "2":
+                        es.RemoverVeiculo();
+                        break;
+
+                    case "3":
+                        es.ListarVeiculos();
+                        break;
+
+                    case "4":
+                        menu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Digite uma opção válida");
+                        break;
+                }
+
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
